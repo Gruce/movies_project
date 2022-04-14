49e5f8951dfbc5ff43a8e4dbef29bda315c916eb
Ahmed <mr.darkboss1@gmail.com>
2022-04-14 04:09:46 +0300

    Hi

---
+++ app/View/Components/UI/Button.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\View\Components\UI;
+
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+
+    public $color;
+    public $default;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($color = "primary"){
+        $this->default = 'text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none ';
+
+        $this->color = $color;
+
+        switch ($this->color){
+            case "primary":
+                $this->default .= 'bg-blue-700 hover:bg-blue-800 focus:ring-blue-300';
+                break;
+            case "secondary":
+                $this->default .= 'bg-gray-700 hover:bg-gray-800 focus:ring-gray-300';
+                break;
+            case "success":
+                $this->default .= 'bg-green-700 hover:bg-green-800 focus:ring-green-300';
+                break;
+            case "error":
+                $this->default .= 'bg-red-700 hover:bg-red-800 focus:ring-red-300';
+                break;
+            case "warning":
+                $this->default .= 'bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-200';
+                break;
+        }
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.u-i.button');
+    }
+}
---
+++ resources/views/components/u-i/button.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => $default]) }} role="button">
+{{$slot}}
+</div>
