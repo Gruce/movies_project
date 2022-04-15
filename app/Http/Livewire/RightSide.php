061f03c9326186aa7ca8d9e78f5ec4694f72e926
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-04-15 04:50:24 +0300

    e

---
+++ app/Http/Livewire/RightSide.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RightSide extends Component
+{
+    public function render()
+    {
+        return view('livewire.right-side');
+    }
+}
