d3c1541c566460a2d11f19a0e4fb25214c3b16c1
Ahmed <mr.darkboss1@gmail.com>
2022-04-15 18:15:53 +0300

    small movie cover

--- resources/views/components/u-i/small-cover.blade.php
+++ resources/views/components/u-i/small-cover.blade.php
@@ -1,4 +1,11 @@
-<div class="w-72 h-40 bg-black rounded-lg flex">
-    <div class="h-full w-28 bg-red-900"></div>
-
+<div class="max-w-sm bg-white rounded-lg p-2 flex mt-3 hover:bg-gray-100">
+         <img class="rounded-lg h-24 basis-1/3" src="/img/inv.jpg" />
+    <div class="px-2 w-full flex flex-col items-start basis-2/3">
+            <h1 class="font-semibold text-left">Avangers Avangers </h1>
+            <p class="text-xs text-gray-500">Action , Drama</p>
+            <div class="flex mt-1">
+                <span class="bg-yellow-300   text-gray-800 text-2xs   mr-1 px-1 py-0.25 rounded ">IMDB</span>
+                <p class="text-xs"> 7.2 </p>
+            </div>
+    </div>
 </div>
