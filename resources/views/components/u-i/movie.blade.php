b90c24035feffe9c19d0aab7fe81134a385ca17c
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-15 23:54:36 +0300

    Update movie.blade.php

--- resources/views/components/u-i/movie.blade.php
+++ resources/views/components/u-i/movie.blade.php
@@ -1,4 +1,4 @@
-    <div class="w-52 h-80 bg-cover flex items-end justify-center  rounded-lg relative group transition ease-in-out duration-300"
+    <div class="w-52 h-80 bg-cover flex items-end justify-center rounded-lg relative group transition ease-in-out duration-300"
         style="background-image: url('{{$imgUrl}}')">
         <div class="w-full h-full absolute bg-gradient-to-b  to-gray-800 from-transparent rounded-lg group-hover:to-gray-900 group-hover:duration-300"></div>
         <div class="z-10 -mb-3 text-center group-hover:mb-5 group-hover:duration-300 ">
@@ -9,8 +9,13 @@
                         <i class="fa-solid fa-star @if ($item <= $rating) text-yellow-300 @else text-gray-300 @endif"></i>
                     @endforeach
                 </div>
-                <x-ui.button href="{{$url}}" color="error" class="mt-3 invisible opacity-0 group-hover:opacity-100 group-hover:visible group-hover:duration-300">Watch Now</x-ui.button>
+                <div class="flex gap-1 mt-3 invisible opacity-0 group-hover:opacity-100 group-hover:visible group-hover:duration-300">
+                    <x-ui.button href="{{$url}}" color="error">Watch Now</x-ui.button>
+                </div>
             </div>
         </div>
+        <x-ui.button color="secondary" class="top-0 left-0 absolute text-2xs py-3 px-3 text-white flex items-center" href="#">
+            <i class="fa-solid fa-clock fa-2x"></i>
+        </x-ui.button>
     </div>
 
