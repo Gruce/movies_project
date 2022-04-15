07c4b81d9f12f92c8cf2acfbdf796435958d22d9
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-04-15 18:21:37 +0300

    yoo

---
+++ app/Http/Livewire/Movies/ShowMovie.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Movies;
+
+use Livewire\Component;
+
+class ShowMovie extends Component
+{
+    public function render()
+    {
+        return view('livewire.movies.show-movie');
+    }
+}
