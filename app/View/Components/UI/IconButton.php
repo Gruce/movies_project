0b1568bef1e8f9ffe8a200d6fb46b67610b785eb
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-04-15 03:29:22 +0300

    A

---
+++ app/View/Components/UI/IconButton.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\UI;
+
+use Illuminate\View\Component;
+
+class IconButton extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.u-i.icon-button');
+    }
+}
