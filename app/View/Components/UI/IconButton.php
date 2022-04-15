6c0a8b3f7091a3b4ca7cfde37619762ceefeb941
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-15 18:32:16 +0300

    asd

--- app/View/Components/UI/IconButton.php
+++ app/View/Components/UI/IconButton.php
@@ -9,39 +9,40 @@ class IconButton extends Component
     // public $color;
     // public $default;
     public $icon;
+    public $default;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    // public function __construct($color = "primary"){
-    //     $this->default = 'text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none ';
-
-    //     if ($this->color = $color){  
-
-    //     switch ($this->color){
-    //         case "primary":
-    //             $this->default .= 'bg-blue-500 hover:bg-blue-800 focus:ring-blue-300';
-    //             break;
-    //         case "secondary":
-    //             $this->default .= 'bg-gray-700 hover:bg-gray-800 focus:ring-gray-300';
-    //             break;
-    //         case "success":
-    //             $this->default .= 'bg-green-700 hover:bg-green-800 focus:ring-green-300';
-    //             break;
-    //         case "error":
-    //             $this->default .= 'bg-red-700 hover:bg-red-800 focus:ring-red-300';
-    //             break;
-    //         case "warning":
-    //             $this->default .= 'bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-200';
-    //             break;
-    //     }
-    //   }
-    //     else 
-    //         $this->default .= 'bg-red-500 hover:bg-red-800 focus:ring-red-300';  
-    // }
-    public function __construct($icon = null)
+    public function __construct($color = "primary", $icon = null)
     {
+
+        // COLOR PROPERTIES
+        $this->default = 'flex items-center justify-center border hover:text-white focus:ring-4 focus:outline-none font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center aspect-square ';
+
+        if ($this->color = $color) {
+            switch ($this->color) {
+                case "primary":
+                    $this->default .= 'text-red-500  border-red-500 hover:bg-red-500 focus:ring-red-300';
+                    break;
+                case "secondary":
+                    $this->default .= 'text-gray-700  border-gray-700 hover:bg-gray-700 focus:ring-gray-300';
+                    break;
+                case "success":
+                    $this->default .= 'text-green-700  border-green-700 hover:bg-green-800 focus:ring-green-300';
+                    break;
+                case "error":
+                    $this->default .= 'text-red-700  border-red-700 hover:bg-red-700 focus:ring-red-300';
+                    break;
+                case "warning":
+                    $this->default .= 'text-yellow-400  border-yellow-500 hover:bg-yellow-500 focus:ring-yellow-200';
+                    break;
+            }
+        } else $this->default .= 'text-red-500  border-red-500 hover:bg-red-500 focus:ring-red-300';
+
+
+        //  ICON PROPERTIES
         if ($icon)
             $this->icon = $icon;
         else
@@ -57,8 +58,8 @@ class IconButton extends Component
     {
         return view('components.u-i.icon-button');
     }
-//     public function render()
-//     {
-//     return view('components.u-i.button');
-//     }
- }
+    //     public function render()
+    //     {
+    //     return view('components.u-i.button');
+    //     }
+}
--- resources/views/components/u-i/icon-button.blade.php
+++ resources/views/components/u-i/icon-button.blade.php
@@ -1,4 +1,4 @@
 
-<button {{ $attributes->merge(['class' => "flex items-center justify-center text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center aspect-square"]) }} type="button">
-<i class="{{$icon}}"></i>
+<button {{ $attributes->merge(['class' => $default]) }} type="button">
+    <i class="{{$icon}}"></i>
 </button>
