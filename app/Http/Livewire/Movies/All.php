a0ac62115514d026fa7fc415232f0e7e12f48493
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-15 22:41:15 +0300

    asd

---
+++ app/Http/Livewire/Movies/All.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Movies;
+
+use Livewire\Component;
+
+class All extends Component
+{
+    public function render()
+    {
+        return view('livewire.movies.all');
+    }
+}
