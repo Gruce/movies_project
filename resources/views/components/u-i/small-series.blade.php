6590c4aa4b89517f31d005b9fc8de5f8ad999811
Ahmed <mr.darkboss1@gmail.com>
2022-04-16 14:39:34 +0300

    Small Series Cover - Right side

---
+++ resources/views/components/u-i/small-series.blade.php
@@ -0,0 +1,26 @@
+<div href="#" class="bg-white rounded-lg p-2 flex mt-3 hover:bg-gray-100 group transition ease-in-out duration-300">
+    <img class="rounded-lg h-28 basis-1/3 w-1/3" src="{{$imgUrl}}" />
+
+    <div class="px-2 flex flex-col items-start justify-between basis-2/3 w-2/3">
+        <div class="flex flex-col items-start">
+            <h1 class="font-semibold text-left">{{$name}}</h1>
+            <p class="text-xs text-gray-500">{{$category}}</p>
+            <div class="flex mt-1">
+                <span class="bg-yellow-300 text-gray-800 text-2xs mr-1 px-1 py-0.25 rounded ">IMDB</span>
+                <p class="text-xs"> {{$rating}} </p>
+            </div>
+        </div>
+        <div class="flex mt-1">
+            <span class="bg-gray-100 text-gray-700 text-2xs mr-1 px-1 py-0.25 rounded ">Season</span>
+            <p class="text-xs"> 3 </p>
+        </div>
+        <div class="flex justify-between gap-1 invisible opacity-0 group-hover:opacity-100 group-hover:visible group-hover:duration-300">
+            <x-ui.button color="error" class="text-2xs py-0.5 px-1 text-white" href="#">
+                Watch
+            </x-ui.button>
+            <x-ui.button color="secondary" class="text-2xs py-0.5 px-1 text-white" href="#">
+                <i class="fa-solid fa-clock"></i>
+            </x-ui.button>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/right-side.blade.php
+++ resources/views/livewire/right-side.blade.php
@@ -20,6 +20,7 @@
             <h1 class="text-left text-gray-500 text-lg font-bold"> Popular Movies</h1>
             <div class="flex flex-col">
                 <x-ui.small-cover name="Avengers" rating="10" imgUrl="/img/inv.jpg" category="Action , Drama" url="#"></x-ui.small-cover>
+                <x-ui.small-series name="Avengers" rating="10" imgUrl="/img/Vik.jpg" category="Action , Drama" url="#"></x-ui.small-cover>
             </div>
 
             <x-ui.button color="error" class="mt-3 text-white block" href="#">
