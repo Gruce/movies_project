4ab4bc7a535945c0598be7572c2cc2fdb00aa816
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-16 18:33:18 +0300

    asd

--- app/Http/Livewire/Ui/Movie.php
+++ app/Http/Livewire/Ui/Movie.php
@@ -7,11 +7,6 @@ use Livewire\Component;
 class Movie extends Component
 {
     public $name, $rating, $imgUrl, $url;
-    public function mount($rating)
-    {
-        $this->rating = round($rating/2);
-    }
-
 
     public function render()
     {
--- app/Http/Livewire/Ui/Series.php
+++ app/Http/Livewire/Ui/Series.php
@@ -8,10 +8,6 @@ class Series extends Component
 {
     public $name, $rating, $imgUrl, $url;
 
-    public function mount($rating){
-        $this->rating = round($rating/2);
-    }
-
     public function render()
     {
         return view('livewire.ui.series');
