c76725b5ed6e401c66a1f28484a32066efb8f802
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-04-16 23:38:05 +0300

    Search button fix

--- resources/views/livewire/right-side.blade.php
+++ resources/views/livewire/right-side.blade.php
@@ -11,7 +11,7 @@
                 </svg>
             </div>
             <input type="text" id="table-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-10 p-2.5"
                 placeholder="Search" />
         </div>
     </div>
