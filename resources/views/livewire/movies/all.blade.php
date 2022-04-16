aa5dd17dcca0a54fac7eef0e04a06a12aa3593bf
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-04-16 23:45:45 +0300

    drop down

--- resources/views/livewire/movies/all.blade.php
+++ resources/views/livewire/movies/all.blade.php
@@ -2,7 +2,7 @@
 <div class="flex gap-2">
     <button id="dropdownDefault" data-dropdown-toggle="dropdown" class="text-black bg-gray-100 hover:bg-gray-200 focus:ring-4  focus:text-red-500 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center " type="button">Genres<svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
     <!-- Dropdown menu -->
-    <div id="dropdown" class="hidden z-20 w-44 bg-gray-50 rounded divide-y divide-gray-100 shadow ">
+    <div id="dropdown" class="hidden z-20 w-44 bg-gray-50 rounded divide-y divide-gray-100 shadow">
         <ul class="py-1 text-sm text-gray-900 dark:text-gray-200" aria-labelledby="dropdownDefault">
         <li>
             <a href="#" class="block py-2 px-4 hover:bg-gray-200">Action</a>
