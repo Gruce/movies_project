abd00f36f6635d6f009a8642a6c6759ad664859d
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-18 02:45:03 +0300

    asd

--- resources/views/livewire/home/best-series.blade.php
+++ resources/views/livewire/home/best-series.blade.php
@@ -4,12 +4,12 @@
             <span>Best Series</span>
         </div>
         <div>
-            <x-ui.icon-button icon="fa-solid fa-angle-left" class="w-8 h-8 text-xl" @click="$refs.slider.scrollLeft -= 2*$refs.slider.firstElementChild.offsetWidth" />
-            <x-ui.icon-button icon="fa-solid fa-angle-right" class="w-8 h-8 text-xl" @click="$refs.slider.scrollLeft += 2*$refs.slider.firstElementChild.offsetWidth" />
+            <x-ui.icon-button color="error" icon="fa-solid fa-angle-left" class="w-8 h-8 text-xl" @click="$refs.slider.scrollLeft -= 2*$refs.slider.firstElementChild.offsetWidth" />
+            <x-ui.icon-button color="error" icon="fa-solid fa-angle-right" class="w-8 h-8 text-xl" @click="$refs.slider.scrollLeft += 2*$refs.slider.firstElementChild.offsetWidth" />
         </div>
     </div>
 
-    <div class="flex overflow-x-hidden overflow-y-hidden flex-nowrap" x-ref="slider">
+    <div class="flex overflow-y-hidden flex-nowrap pb-2 rounded-lg" x-ref="slider">
         @forelse ($series as $series)
             <div x-ref="slide_item" class="mr-5">
                 {{-- <livewire:ui.series :episode="$series->seasons->first()->episodes->first()->id" :wire:key="$series->id" /> --}}
