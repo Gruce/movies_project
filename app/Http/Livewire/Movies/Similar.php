16e4b5f48953867aba08e21d1104e3bc9ac4d940
hassan_000 <salem.cf11@gmail.com>
2022-04-19 21:06:45 +0300

    I

--- app/Http/Livewire/Movies/Similar.php
+++ app/Http/Livewire/Movies/Similar.php
@@ -3,9 +3,15 @@
 namespace App\Http\Livewire\Movies;
 
 use Livewire\Component;
-
+use App\Models\Movie;
 class Similar extends Component
 {
+    public function mount($movie_id){
+        $movie=Movie::findOrFail($movie_id);
+        $this->movies = Movie::whereHas('genres', function ($query)use($movie,$movie_id) {
+            $query->where('movie_id','!=',$movie_id)->whereIn('genre_id', $movie->genres->pluck('id'));
+        })->orderBy('rating', 'desc')->take(5)->get();
+    }
     public function render()
     {
         return view('livewire.movies.similar');
