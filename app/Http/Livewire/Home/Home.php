2b265f2b0eb9e7e0f03f01a2b1495fd445d3080d
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-04-19 22:57:51 +0300

    add

--- app/Http/Livewire/Home/Home.php
+++ app/Http/Livewire/Home/Home.php
@@ -4,7 +4,11 @@ namespace App\Http\Livewire\Home;
 
 use Livewire\Component;
 use Illuminate\Http\Request;
-use App\Models\Movie;
+use App\Models\{
+    Movie,
+    Series
+};
+
 class Home extends Component
 {
 
@@ -12,6 +16,40 @@ class Home extends Component
     protected $queryString = ['type'];
 
     public function mount(){
+        $this->last = null;
+
+        if($this->type === 'movies'){
+            $this->last = Movie::with('cover')->get(['id']);
+            // dd($this->last->toArray());
+        }
+
+        elseif($this->type==='series'){
+            $this->last = Series::with(
+                [
+                    'seasons' => function($season){
+                        return $season->with(
+                            [
+                                'episodes' => function($episode){
+                                    return $episode->with(
+                                        [
+                                            'cover' => function($cover){
+                                                return $cover->latest()->take(1);
+                                            }
+                                        ]
+                                        );
+                                }
+                            ]
+                        );
+                    }
+                ]
+                )->get(['id']);
+            dd($this->last->toArray());
+        }
+
+        else{
+            abort(404);
+        }
+
         if (!request()->query('type')){
             return redirect()->route('home', ['type' => 'movies']);
         }
