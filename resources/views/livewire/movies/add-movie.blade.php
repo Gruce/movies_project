e0c9339f89f65436237d68d300d513426c82acb0
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-04-20 16:01:10 +0300

    add

--- resources/views/livewire/movies/add-movie.blade.php
+++ resources/views/livewire/movies/add-movie.blade.php
@@ -1,36 +1,36 @@
 <div>
     <div class="flex flex-col">
         <div class="grid h-10 card">
-            <h2 class="text-left card-title  pr-4"> Add Movie</h2>
+            <h2 class="font-bold text-l text-red-700 text-left card-title  pr-4"> Add Movie</h2>
         </div>
     </div>
     <form wire:submit.prevent="add">
 
-        <div class="grid grid-cols-2 gap-4">
-            <div class="mb-6">
-                <label for="name" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">movie
+        <div class="grid grid-cols-2 gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+                <label for="name" class="text-left block mb-2 text-sm font-medium text-gray-900">movie
                     name</label>
                 <input wire:model="name" type="text" id="name"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                    class="bg-transparent border-0 border-b-2 border-black text-gray-900 text-sm block w-full p-2.5 focus:ring-gray-300 focus:border-black"
                     placeholder="name" >
                     @error('name') <span class="error text-red-600">{{ $message }}</span> @enderror
             </div>
             <!-- ... -->
             <div class="mb-6">
-                <label for="rating" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                <label for="rating" class="text-left block mb-2 text-sm font-medium text-gray-900">
                     rating</label>
-                <input wire:model="rating" type="number"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                <input wire:model="rating" type="number" step="0.1" min="0" max="10"
+                    class="bg-transparent border-0 border-b-2 border-red-500 text-gray-900 text-sm block w-full p-2.5 focus:ring-blue-300 focus:border-blue-700"
                     placeholder="rating">
                     @error('rating') <span class="error text-red-600">{{ $message }}</span> @enderror
             </div>
         </div>
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-2 gap-6">
             <div class="mb-6">
                 <label for="name"
                     class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Duration</label>
                 <input wire:model="duration" type="number"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                    class="bg-transparent border-0 border-b-2 border-red-500 text-gray-900 text-sm block w-full p-2.5 focus:ring-blue-300 focus:border-blue-700"
                     placeholder="Duration">
                     @error('duration') <span class="error text-red-600">{{ $message }}</span> @enderror
             </div>
@@ -39,7 +39,7 @@
                 <label class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                     Release Date</label>
                 <select wire:model="release_date"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    class="bg-transparent border-0 border-b-2 border-black-500 text-gray-900 text-sm focus:ring-blue-300 focus:border-blue-700 block w-full p-2.5">
                     <option selected value="" > select year </option>
                     @for ($year = 1800; $year<=date('Y'); $year++)
                     <option value ="{{$year}}" >{{$year}}</option>
@@ -51,9 +51,9 @@
 
         <div class="mt-4 grid grid-cols-1 gap-4 ">
             <label for="message"
-                class="text-left block text-sm font-medium text-gray-900 dark:text-gray-400">Description</label>
+                class="text-left block text-sm font-medium text-gray-900">Description</label>
             <textarea wire:model="description" id="message" rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-red-500 focus:ring-red-800 focus:border-red-600"
                 placeholder="description..."></textarea>
                 @error('description') <span class="error text-red-600">{{ $message }}</span> @enderror
         </div>
@@ -104,4 +104,41 @@
         </x-ui.button> --}}
     </form>
 
+{{--     
+<form>
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_email" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_repeat_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+    </div>
+    <div class="grid xl:grid-cols-2 xl:gap-6">
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_first_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_last_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+    </div>
+    </div>
+    <div class="grid xl:grid-cols-2 xl:gap-6">
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_phone" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+    <input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="">
+    <label for="floating_company" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
+    </div>
+    </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+     --}}
 </div>
