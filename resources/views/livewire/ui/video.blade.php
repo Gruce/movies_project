bc9e19c788a76cf64da06141be6a4ef1f23d8bcf
Hassan Alkhalidi <gruceing@gmail.com>
2022-04-23 00:07:20 +0300

    asd

--- resources/views/livewire/ui/video.blade.php
+++ resources/views/livewire/ui/video.blade.php
@@ -1,17 +1,32 @@
 <div>
     <link href="https://vjs.zencdn.net/7.18.1/video-js.css" rel="stylesheet" />
 
-    <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
-        poster="{{ $file->fileable->cover_url }}" data-setup="{}">
-        <source src="{{ asset('storage/' . $file->path) }}" type="video/mp4" />
-        {{-- <source src="MY_VIDEO.webm" type="video/webm" /> --}}
-        <p class="vjs-no-js">
-            To view this video please enable JavaScript, and consider upgrading to a
-            web browser that
-            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-        </p>
+    <video id="my-video" class="video-js" width="640" height="264" data-setup="{}">
+        <track src="http://example.com/oceans.vtt" kind="captions" srclang="en" label="English">
+        <track src="http://example.com/oceans.vtt" kind="captions" srclang="ar" label="Arabic">
     </video>
 
 
     <script src="https://vjs.zencdn.net/7.18.1/video.min.js"></script>
+
+    <script>
+        var player = videojs('my-video', {
+            fluid: true,
+            controls: true,
+            autoplay: true,
+            preload: 'auto',
+            playbackRates: [0.7, 1.0, 1.5, 2.0],
+            sources: [{
+                src: '{{ asset('storage/' . $file->path) }}',
+                type: 'video/mp4'
+            }],
+            poster: '{{ $file->fileable->slider_url }}',
+            language: '{{ app()->getLocale() }}',
+            textTrackDisplay: {
+                mode: 'hidden'
+            },
+            notSupportedMessage: 'Your browser is not supported',
+            responsive: true,
+        });
+    </script>
 </div>
